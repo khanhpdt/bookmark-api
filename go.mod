56aea665583fa74efc6bdbfe457617d583d82966
Khanh Pham <khanhpdtvn@gmail.com>
2020-04-13 23:30:00 +0700

    CORS handling

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module khanhpdt.vn/bookmarkapi
 
 go 1.13
 
-require github.com/gin-gonic/gin v1.6.2
+require (
+	github.com/gin-contrib/cors v1.3.1 // indirect
+	github.com/gin-gonic/gin v1.6.2
+)
--- src/app.go
+++ src/app.go
@@ -5,11 +5,20 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
+
+	// - No origin allowed by default
+	// - GET,POST, PUT, HEAD methods
+	// - Credentials share disabled
+	// - Preflight requests cached for 12 hours
+	config := cors.DefaultConfig()
+	config.AllowOrigins = []string{"http://localhost:8080"} // for development
+	r.Use(cors.New(config))
 
 	r.MaxMultipartMemory = 8 << 20 // 8 MB (default is 32 MB)
 
@@ -21,15 +30,16 @@ func main() {
 			return
 		}
 
-		log.Println()
 		filePath := fmt.Sprintf("/tmp/%s", file.Filename)
-		fmt.Printf("Saving file %s to %s", file.Filename, filePath)
 
 		if err := c.SaveUploadedFile(file, filePath); err != nil {
-			c.String(http.StatusBadRequest, fmt.Sprintf("Error saving file to %s: %s", filePath, err.Error()))
+			errMsg := fmt.Sprintf("Error saving file to %s: %s", filePath, err.Error())
+			log.Printf(errMsg)
+			c.String(http.StatusBadRequest, errMsg)
 			return
 		}
 
+		log.Printf("Saved file %s.", filePath)
 		c.String(http.StatusOK, fmt.Sprintf("File %sfile uploaded.", file.Filename))
 	})
 
