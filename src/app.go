639003cc2d3fddd67a5d127d7e7834a45296f9a0
Khanh Pham <khanhpdtvn@gmail.com>
2020-04-13 23:20:07 +0700

    Initial code to receive uploaded file

--- src/app.go
+++ src/app.go
@@ -1,13 +1,37 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
+
+	r.MaxMultipartMemory = 8 << 20 // 8 MB (default is 32 MB)
+
+	r.POST("/upload", func(c *gin.Context) {
+		file, err := c.FormFile("file")
+
+		if err != nil {
+			c.String(http.StatusBadRequest, fmt.Sprintf("Error receiving file: %s", err.Error()))
+			return
+		}
+
+		log.Println()
+		filePath := fmt.Sprintf("/tmp/%s", file.Filename)
+		fmt.Printf("Saving file %s to %s", file.Filename, filePath)
+
+		if err := c.SaveUploadedFile(file, filePath); err != nil {
+			c.String(http.StatusBadRequest, fmt.Sprintf("Error saving file to %s: %s", filePath, err.Error()))
+			return
+		}
+
+		c.String(http.StatusOK, fmt.Sprintf("File %sfile uploaded.", file.Filename))
 	})
+
 	r.Run(":8081") // listen and serve on 0.0.0.0:8081
 }
